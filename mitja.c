6faa9ea6a338641609d26370300401ef4bf59667
saezeric <saezescalonaeric@fpllefia.com>
2023-10-03 16:27:11 +0200

    prgit-saez24 v1

---
+++ mitja.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+	float suma=0;
+	float mitja;
+	float nou_valor;
+	int numero_repeticions=0;
+	char consulta='s';
+	
+	while(consulta=='s') {
+		printf("Saez24 Dòna la següent dada: ");
+		scanf("%f",&nou_valor);
+		suma=suma+nou_valor;
+		numero_repeticions=numero_repeticions+1;
+		printf("Saez24 Vols continuar (s/n): ");
+		scanf(" %c",&consulta); 
+	}
+	mitja=suma/numero_repeticions;
+	printf("Saez24 La mitja és: %.2f\n",mitja);	
+	return 0;
+}
+
